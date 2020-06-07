71080189fa17c9f3ce4a566f09792abce556ae0f
Laura Beerden <60176579+BLaura1995@users.noreply.github.com>
2020-06-07 12:12:43 +0200

    Update

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
-$(document).ready(function() {
-    $("#sectionUp").hide();
-    $(".btn1").click(function() {
-        $("#sectionUp").slideDown(900);
+// $(document).ready(function() {
+//     $("#sectionUp").hide();
+//     $(".btn1").click(function() {
+//         $("#sectionUp").slideDown(900);
 
+//     });
+// });
+$(document).ready(function() {
+    $(".content").hide();
+    $(".set > a").on("click", function() {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(this)
+          .siblings(".content")
+          .slideUp(200);
+        $(".set > a i")
+          .removeClass("fa-minus")
+          .addClass("fa-plus");
+      } else {
+        $(".set > a i")
+          .removeClass("fa-minus")
+          .addClass("fa-plus");
+        $(this)
+          .find("i")
+          .removeClass("fa-plus")
+          .addClass("fa-minus");
+        $(".set > a").removeClass("active");
+        $(this).addClass("active");
+        $(".content").slideUp(200);
+        $(this)
+          .siblings(".content")
+          .slideDown(200);
+      }
     });
-});
+  });
+  
